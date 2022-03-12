b41f0578c3de07fb4d7c6c08722fb891ad324569
tomi2107 <tom.palmeyro@gmail.com>
2022-03-12 20:55:01 -0300

    Crud php

---
+++ guardarDatosEditados.php
@@ -0,0 +1,36 @@
+<?php
+
+#Salir si alguno de los datos no está presente
+if(
+	!isset($_POST["nombre"]) || 
+	!isset($_POST["apellidos"]) || 
+	!isset($_POST["sexo"]) || 
+	!isset($_POST["id"])
+) exit();
+
+#Si todo va bien, se ejecuta esta parte del código...
+
+include_once "base_de_datos.php";
+$id = $_POST["id"];
+$nombre = $_POST["nombre"];
+$apellidos = $_POST["apellidos"];
+$sexo = $_POST["sexo"];
+
+$sentencia = $base_de_datos->prepare("UPDATE personas SET nombre = ?, apellidos = ?, sexo = ? WHERE id = ?;");
+$resultado = $sentencia->execute([$nombre, $apellidos, $sexo, $id]); # Pasar en el mismo orden de los ?
+if($resultado === TRUE) echo "Cambios guardados";
+else echo "Algo salió mal. Por favor verifica que la tabla exista, así como el ID del usuario";
+?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <br>
+	<a href="index.html"><button type="text">Principio</button></a>
+</body>
+</html>
